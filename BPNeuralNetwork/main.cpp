094b8f114b1931b916fc3d95f278cf7f7fa2806e
heretic1993 <wuxingfang@gmail.com>
2015-04-08 11:11:39 +0800

    First commit

---
+++ BPNeuralNetwork/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  BPNeuralNetwork
+//
+//  Created by 吴行方 on 4/8/15.
+//  Copyright (c) 2015 Holden WU. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
